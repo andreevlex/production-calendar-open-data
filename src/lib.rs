05c85ae3d9bbfae3a327d1f65f9cfb50c4e583fa
Andreev Alexander <andreevlex.as@gmail.com>
2017-11-11 21:42:42 +0300

    Заготовка библиотеки

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+extern crate csv;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
